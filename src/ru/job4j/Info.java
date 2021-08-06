08ea4dd9d7d8b261d56864b901add43ffcdc84cc
Saper52 <saperrvia1@mail.ru>
2021-08-07 00:22:22 +0300

    Задача 8.

--- src/ru/job4j/Info.java
+++ src/ru/job4j/Info.java
@@ -3,5 +3,9 @@ package ru.job4j;
 public class Info {
     public static void main (String[] args) {
         System.out.println("04.08.2021");
+        int first = 1;
+        int second = 2;
+        int result = first + second;
+        System.out.println(result);
     }
 }
