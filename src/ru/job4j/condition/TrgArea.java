493ea8356c4b46a4f47c428654e528046101d907
Saper52 <saperrvia1@mail.ru>
2021-08-07 00:30:41 +0300

    Задача 9.

---
+++ src/ru/job4j/condition/TrgArea.java
@@ -0,0 +1,12 @@
+package ru.job4j.condition;
+
+public class TrgArea {
+    public static double Sqrt(){
+        double rsl = -1;
+        return rsl;
+    }
+    public static void main (String [] args){
+        double result = TrgArea.Sqrt(1, 3);
+        System.out.println("Хелов");
+    }
+}
