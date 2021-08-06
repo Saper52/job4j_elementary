ef2320a13f346518e14a949cf70dbb49ecccc5d6
Saper52 <saperrvia1@mail.ru>
2021-08-07 00:40:14 +0300

    Задача 9.

--- src/ru/job4j/condition/TrgArea.java
+++ src/ru/job4j/condition/TrgArea.java
@@ -1,12 +1,15 @@
 package ru.job4j.condition;
 
+import ru.job4j.calculator.MathFunc;
+
 public class TrgArea {
-    public static double Sqrt(){
-        double rsl = -1;
+    public static double area(double a, double b, double c){
+        double p = (a+b+c)/2;
+        double rsl = Math.sqrt(p*(p-a)*(p-b)*(p-c));
         return rsl;
     }
     public static void main (String [] args){
-        double result = TrgArea.Sqrt(1, 3);
-        System.out.println("Хелов");
+        double result = TrgArea.area(3, 3, 3);
+        System.out.println("Хелов, площадь треугольника равна " +result);
     }
 }
