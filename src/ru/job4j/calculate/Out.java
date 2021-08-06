1cf95306969ff5fe1c1bd2481a12b11c5a1b86b1
Saper52 <saperrvia1@mail.ru>
2021-08-07 00:51:32 +0300

    Задача 9.1

---
+++ src/ru/job4j/calculate/Out.java
@@ -0,0 +1,14 @@
+package ru.job4j.calculate;
+
+public class Out {
+    public static void main (String [] args){
+        String lyrics = "На улице лето";
+        System.out.println(lyrics);
+        String name = "Ivan";
+        String surname = "Inanov";
+        String otchestvo = "Ivanych";
+        String probel = " ";
+        String id = name+probel+surname+probel+otchestvo;
+        System.out.println(id);
+    }
+}
