b6f5b15809b9c2c0468b787b5c107313bedf5eb5
Saper52 <saperrvia1@mail.ru>
2022-07-29 00:12:19 +0300

    0. Что такое переменная. [#3610 #326806]

---
+++ src/main/java/ru/job4j/Variables.java
@@ -0,0 +1,22 @@
+package ru.job4j;
+
+public class Variables {
+    public static void main(String[] args) {
+        byte mem = 127;
+        System.out.println(mem);
+        short size = 32767;
+        System.out.println(size);
+        int length = 2147483647;
+        System.out.println(length);
+        long money = 1234567890123456789L;
+        System.out.println(money);
+        float weight = 1.054524245245245F;
+        System.out.println(weight);
+        double distance = 100500.99;
+        System.out.println(distance);
+        boolean exists = true;
+        System.out.println(exists);
+        char gender = 'Y';
+        System.out.println(gender);
+    }
+}
