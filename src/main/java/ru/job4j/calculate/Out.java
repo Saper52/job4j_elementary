93e14b1e0a9cac4589295265b709967208faae8f
Saper52 <saperrvia1@mail.ru>
2022-07-30 12:53:14 +0300

    9.1. String [#237407]

---
+++ src/main/java/ru/job4j/calculate/Out.java
@@ -0,0 +1,15 @@
+package ru.job4j.calculate;
+
+public class Out {
+    public static void main(String[] args) {
+        String name = "Alexander";
+        String surname = "Ryabkov";
+        String separate = " ";
+        byte age = 30;
+        System.out.println(name + separate + age + " years old");
+        String label = "I ";
+        label += "like ";
+        label += "Java!";
+        System.out.println(label);
+    }
+}
