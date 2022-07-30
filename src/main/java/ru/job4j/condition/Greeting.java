219b9c6b53278066fa7e57a954f138113890a98b
Saper52 <saperrvia1@mail.ru>
2022-07-30 14:01:40 +0300

    9.3. Преобразование примитивных типов [#383106 #327140]

--- src/main/java/ru/job4j/condition/Greeting.java
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -8,5 +8,16 @@ public class Greeting {
         int year = 2022;
         idea += year;
         System.out.println(idea);
+        int i = 10000;
+        byte b = (byte) i;
+        short s = (short) i;
+        System.out.println("Вывод в консоль значения после преобразования: " + b);
+        System.out.println("Вывод в консоль значения после преобразования: " + s);
+        double d = 94.984751;
+        int ii = (int) Math.round(d);
+        short ss = 1500;
+        char cc = (char) ss;
+        double dd = cc;
+        System.out.println("Вывод в консоль значения после преобразования: " + dd);
     }
 }
