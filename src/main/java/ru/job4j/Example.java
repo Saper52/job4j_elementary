21df82cf59a597940399fc57c97de63edc57fc20
saperrvia@mail.ru <Vozesi511>
2022-07-31 14:18:42 +0300

    как работают коммиты и branches git - Remote branches origin/master

--- src/main/java/ru/job4j/Example.java
+++ src/main/java/ru/job4j/Example.java
@@ -2,6 +2,6 @@ package ru.job4j;
 
 public class Example {
     public static void main(String[] args) {
-        System.out.println("Привет мир! проверка кодировки ааааaaаа");
+        System.out.println("Привет мир! проверка кодировки ааааaааaаа");
     }
 }
