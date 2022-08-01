39026383242245b4f63f77264f5abc4a32042aa2
Saper52 <saperrvia1@mail.ru>
2022-08-01 23:50:38 +0300

    4.1.1. Операторы сравнения в String. [#237406] CheckStyle edit

--- src/main/java/ru/job4j/condition/StringEq.java
+++ src/main/java/ru/job4j/condition/StringEq.java
@@ -5,12 +5,12 @@ public class StringEq {
         String first = new String("two");
         String one = "one";
         String three = "lalalal";
-        if (first==one){
+        if (first == one) {
             System.out.println("first equals one (compare through ==)");
         } else {
             System.out.println("first does not equal one (compare through ==)");
         }
-        if (first.equals(three)){
+        if (first.equals(three)) {
             System.out.println("first equals three");
         } else {
             System.out.println("first does not equal three");
