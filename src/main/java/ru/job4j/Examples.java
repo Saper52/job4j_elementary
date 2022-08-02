d1b3f4f6ef4d3eb36967fc9be5ffa912db52f57d
Saper52 <saperrvia1@mail.ru>
2022-08-02 19:21:29 +0300

    4.1.3. Блок-схемы [#313435 #328166]

--- src/main/java/ru/job4j/Examples.java
+++ src/main/java/ru/job4j/Examples.java
@@ -1,7 +1,23 @@
 package ru.job4j;
 
 public class Examples {
+    public static void maks(int a, int b, int c) {
+        if (a > b) {
+            if (a > c) {
+                System.out.println("Max number - a");
+            } else {
+                System.out.println("Max number - c");
+            }
+        } else {
+            if (c > b) {
+                System.out.println("Max number - c");
+            } else {
+                System.out.println("Max number - b");
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Привет мир!Проверка кодировки");
+        Examples.maks(4, 6, 7);
     }
 }
