5587b1aaee3f53748c7b88f601ee75049c396b61
alexander.ryabkov <saperrvia1@mail.ru>
2022-08-10 01:16:31 +0300

    5.7. Ипотека [#156315]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -4,7 +4,7 @@ public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int count = 0;
         while (amount > 0) {
-            amount = amount * percent / 100 + amount - salary;
+            amount += amount * percent / 100 - salary;
             count++;
         }
         return count;
